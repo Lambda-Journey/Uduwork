1af50e700e3d9e19ff841fa3d22d3cfd25fc9fa1
SebastianGarces <gsgarces1@gmail.com>
2019-09-08 01:09:13 -0400

    new layout, burger menu

---
+++ dist/js/app.js
@@ -0,0 +1,25 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav__links');
+const navLinks = document.querySelectorAll('.nav__links li')
+
+
+const navSlide = () => {
+    burger.addEventListener('click', () => {
+        //Toggle nav--active
+        nav.classList.toggle('nav--active');
+        //Animation
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 500ms ease forwards ${index / 7 + 0.6}s`;
+            }
+        });
+    });
+}
+
+const app = () => {
+    navSlide(); //burger animation
+}
+
+app();
