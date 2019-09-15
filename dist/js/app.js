a2b870519eeb1af3cab931fdb196865fd8585c32
SebastianGarces <gsgarces1@gmail.com>
2019-09-14 21:57:27 -0400

    loading animations

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,7 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav__links');
-const navLinks = document.querySelectorAll('.nav__links li')
-
-
 const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav__links');
+    const navLinks = document.querySelectorAll('.nav__links li')
     burger.addEventListener('click', () => {
         //Toggle nav--active
         nav.classList.toggle('nav--active');
@@ -18,6 +16,61 @@ const navSlide = () => {
         burger.classList.toggle('close');
     });
 }
+
+const scrollAppearTitle = () => {
+    let title = document.querySelector('#freelancers');
+    let introPosition = title.getBoundingClientRect().top;
+    let screePosition = window.innerHeight / 1.2;
+
+    if (introPosition < screePosition) {
+        title.classList.add('appear');
+    }
+}
+
+window.addEventListener('scroll', scrollAppearTitle);
+
+
+
+const scrollAppearLeftTitle = () => {
+    let lefttitle = document.querySelector('.section-b__container__left');
+    let introPosition = lefttitle.getBoundingClientRect().top;
+    let screePosition = window.innerHeight / 1.2;
+
+    if (introPosition < screePosition) {
+        lefttitle.classList.add('appear');
+    }
+}
+
+const scrollAppearTitle2 = () => {
+    let title2 = document.querySelector('#usuarios');
+    let introPosition = title2.getBoundingClientRect().top;
+    let screePosition = window.innerHeight / 1.2;
+
+    if (introPosition < screePosition) {
+        title2.classList.add('appear');
+    }
+}
+
+window.addEventListener('scroll', scrollAppearLeftTitle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const app = () => {
     navSlide(); //burger animation
